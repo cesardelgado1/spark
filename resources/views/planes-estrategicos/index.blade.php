1a9414900519d94e902dd7b75dd92b5a1a875621
MariPM2 <mariana.perez5@upr.edu>
2025-03-26 11:08:53 -0400

    Changed routes, working on fixing photos

--- resources/views/planes-estrategicos/index.blade.php
+++ resources/views/planes-estrategicos/index.blade.php
@@ -3,11 +3,12 @@
         Planes Estratégicos
     </x-slot:heading>
 
-    <div class="px-6 py-8">
-        <div class="flex flex-col items-center space-y-6">
-            <div class="flex space-x-4 justify-center">
-                <img src="{{ asset('images/sello-upr.png') }}" class="w-[390px]">
-                <img src="{{ asset('images/sello-uprm.png') }}" alt="Plan Estratégico UPRM" class="w-[250px]">
+{{--    <div class="px-6 py-8">--}}
+{{--        <div class="flex flex-col items-center space-y-6">--}}
+{{--            <div class="flex space-x-4 justify">--}}
+            <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 sm:flex sm:justify-between">
+                <img src="{{ asset('images/sello-upr.png') }}">
+                <img src="{{ asset('images/sello-uprm.png') }}" alt="Plan Estratégico UPRM" class="w-[350px]">
             </div>
 
             {{-- Botones --}}
@@ -17,10 +18,10 @@
                 </a>
 
 {{--                No se como mover solamente este boton hacia la derecha :( --}}
-                <a href="#" class="bg-[#1F2937] text-white px-3 py-2 rounded-lg hover:bg-green-700 transition">
+                <a href="#" class="bg-[#1F2937] text-white px-3 py-2 rounded-lg hover:bg-green-700 transition ">
                     Plan Estratégico UPRM
                 </a>
             </div>
-        </div>
-    </div>
+{{--        </div>--}}
+{{--    </div>--}}
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -2,18 +2,24 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/inicio', function () {
-    return view('home');
-});
 
-Route::get('/planes-estrategicos', function () {
-    return view('planes-estrategicos.index');
-});
+Route::view('/', 'home');
+Route::view('/planes-estrategicos', 'planes-estrategicos/index');
+Route::view('/reportes', 'reportes/index');
+Route::view('/configuracion', 'configuracion/index');
 
-Route::get('/reportes', function () {
-    return view('reportes.index');
-});
+//Route::get('/', function () {
+//    return view('home');
+//});
 
-Route::get('/configuracion', function () {
-    return view('configuracion.index');
-});
+//Route::get('/planes-estrategicos', function () {
+//    return view('planes-estrategicos.index');
+//});
+
+//Route::get('/reportes', function () {
+//    return view('reportes.index');
+//});
+//
+//Route::get('/configuracion', function () {
+//    return view('configuracion.index');
+//});
