60ff2695b90ae2c9484240b0bc368d1e864317f9
MariPM2 <mariana.perez5@upr.edu>
2025-03-28 20:36:53 -0400

    Adding the 4 concat columns: Asuntos, Metas, Objetivos, Indicadores

--- app/Exports/StrategicPlanExport.php
+++ app/Exports/StrategicPlanExport.php
@@ -16,7 +16,7 @@ class StrategicPlanExport implements FromCollection, WithHeadings
 //            And T.t_id = G.t_id
 //            AND G.g_id = O.g_id
 //            AND O.o_id = I.o_id
-        
+
         // Query data and return it as a collection
         $data = DB::table('strategic_plans as SP')
             ->join('topics as T', 'SP.sp_id', '=', 'T.sp_id')
@@ -30,6 +30,14 @@ class StrategicPlanExport implements FromCollection, WithHeadings
                 'I.i_id', 'I.i_num', 'I.i_text', 'I.i_type', 'I.i_doc_path', 'I.i_value')
             ->get();
 
+        // Adding the new concatenated columns: Asuntos, Metas, Objetivos, Indicadores
+        foreach ($data as $item) {
+            $item->Asuntos = 'Asunto ' . $item->t_num . ': ' . $item->t_text;
+            $item->Metas = 'Meta ' . $item->g_num . ': ' . $item->g_text;
+            $item->Objetivos = 'Objetivo ' . $item->t_num . '.' . $item->g_num . '.' . $item->o_num . ': ' . $item->o_text;
+            $item->Indicadores = 'Indicador ' . $item->t_num . '.' . $item->g_num . '.' . $item->o_num . '.' . $item->i_num . ': ' . $item->i_text;
+        }
+
         return $data;
     }
 
@@ -42,3 +50,4 @@ class StrategicPlanExport implements FromCollection, WithHeadings
         return $data->isNotEmpty() ? array_keys((array) $data->first()) : [];
     }
 }
+
