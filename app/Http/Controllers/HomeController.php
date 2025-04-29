20ffd3ee51e4c9880780edf9d90c21ab9d74071b
Cesar Delgado <cesardelgado@Cesars-MacBook-Air.local>
2025-04-29 12:32:42 -0400

    added mobile home view

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,6 +9,7 @@ class HomeController extends Controller
 {
     public function index()
     {
+        // Calculate indicators progress
         $total = Indicator::whereHas('objective.goal.topic.strategicplan', function ($query) {
             $query->where('sp_institution', 'UPRM');
         })->count();
@@ -20,6 +21,15 @@ class HomeController extends Controller
         $sinValor = $total - $conValor;
         $porcentaje = $total > 0 ? round(($conValor / $total) * 100, 1) : 0;
 
+        // Check if user is on mobile device
+        $userAgent = request()->header('User-Agent');
+
+        if (preg_match('/mobile/i', $userAgent)) {
+            // If mobile, return the mobile version
+            return view('mobile-home', compact('total', 'conValor', 'sinValor', 'porcentaje'));
+        }
+
+        // Otherwise return normal desktop view
         return view('home', compact('total', 'conValor', 'sinValor', 'porcentaje'));
     }
 }
