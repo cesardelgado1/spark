6d9507e3a7a32c79f24e8cf785ed177d77f48a5f
MariPM2 <mariana.perez5@upr.edu>
2025-04-30 19:22:26 -0400

    Fixing Assignee lock

--- resources/views/indicators/fill.blade.php
+++ resources/views/indicators/fill.blade.php
@@ -32,7 +32,14 @@
                         </p>
 
                         @if($indicator->i_locked)
-                            <p class="text-red-500 font-bold text-sm mb-2">🔒 Este indicador ha sido cerrado. No puedes modificarlo.</p>
+{{--                            <p class="text-red-500 font-bold text-sm mb-2">🔒 Este indicador ha sido cerrado. No puedes modificarlo.</p>--}}
+                            <div class="flex items-center space-x-2 mb-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                    <rect x="3" y="11" width="18" height="11" rx="2" ry="2"></rect>
+                                    <path d="M7 11V7a5 5 0 0 1 10 0v4"></path>
+                                </svg>
+                                <span class="text-red-500 font-bold">Este indicador ha sido cerrado. No puedes modificarlo.</span>
+                            </div>
                         @endif
 
                         @if ($indicator->i_type === 'integer')
