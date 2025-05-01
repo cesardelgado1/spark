4d12d755ec2d5adc38304f46753bf0071525e2f6
cesardelgado1 <cesar.delgado1@upr.edu>
2025-04-30 21:12:33 -0400

    home controller conflict

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,13 +8,23 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-
+    /**
+     * Displays the home page (or mobile version) with indicator progress stats.
+     *
+     * Loads the most recent UPRM strategic plan and calculates:
+     * - Total indicators
+     * - Indicators with values
+     * - Indicators without values
+     * - Completion percentage
+     */
     public function index()
     {
+        // Get latest UPRM strategic plan by starting year (descending)
         $latestPlan = StrategicPlan::where('sp_institution', 'UPRM')
             ->orderByRaw("CAST(SUBSTRING_INDEX(sp_years, '-', 1) AS UNSIGNED) DESC")
             ->first();
 
+        // If no plan found, return default values
         if (!$latestPlan) {
             return view('home', [
                 'total' => 0,
@@ -25,10 +35,12 @@ class HomeController extends Controller
             ]);
         }
 
+        // Count all indicators under the latest plan
         $total = Indicator::whereHas('objective.goal.topic.strategicplan', function ($query) use ($latestPlan) {
             $query->where('sp_id', $latestPlan->sp_id);
         })->count();
 
+        // Count indicators that have a value
         $conValor = Indicator::whereHas('objective.goal.topic.strategicplan', function ($query) use ($latestPlan) {
             $query->where('sp_id', $latestPlan->sp_id);
         })->whereNotNull('i_value')->count();
@@ -36,6 +48,7 @@ class HomeController extends Controller
         $sinValor = $total - $conValor;
         $porcentaje = $total > 0 ? round(($conValor / $total) * 100, 1) : 0;
 
+        // Detect if user is on a mobile device
         $userAgent = request()->header('User-Agent');
         $isMobile = preg_match('/mobile/i', $userAgent);
 
@@ -43,5 +56,4 @@ class HomeController extends Controller
 
         return view($viewName, compact('total', 'conValor', 'sinValor', 'porcentaje', 'latestPlan'));
     }
-
 }
